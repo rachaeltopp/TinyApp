e9dd2a4284c9800a42f95fbae098acdb4776ff65
Rachael Topp <rachaeltopp@Rachaels-MacBook-Air.local>
2017-10-10 16:43:02 -0400

    next major step (url shortening part 1)

--- express_server.js
+++ express_server.js
@@ -1,8 +1,14 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({extended: true}));
+
+function random () {
+  return Math.random().toString(36).slice(2,8);
+};
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -14,9 +20,18 @@ app.get('/urls', (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); //debug statement to see POST parameters
+  res.send("Ok");        //respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
