38674124b009d3d5ff3a335883fcf2d58e61095b
Rachael Topp <rachaeltopp@Rachaels-MacBook-Air.local>
2017-10-14 11:42:09 -0400

    added README.md and screenshots

---
+++ README.md
@@ -0,0 +1,29 @@
+# Tinyapp Project
+
+Tinyapp is a full stack web application built with Node and Express that allows users to shorten long URLs.
+
+## Final Product
+
+![Register](/screenshots/register.png?raw=true)
+![New URL](/screenshots/new-url.png?raw=true)
+![Home Page](/screenshots/home-page.png?raw=true)
+
+## Dependencies
+
+- Node.js
+- Express
+- EJS
+- bcrypt
+- body-parser
+- cookie-session
+
+## Getting Started
+
+- Install all dependencies (using the `npm instal` command).
+- Run the development web server using the `node express_server.js` command.
+
+
+
+
+
+
--- express_server.js
+++ express_server.js
@@ -2,27 +2,28 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
-//const cookieParser = require("cookie-parser");
 const cookieSession = require("cookie-session");
 const bcrypt = require("bcrypt");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-//app.use(cookieParser());
 app.use(cookieSession({
   name: 'session',
   keys: ['userID']
 }));
 
+//function to create random ID
 function random () {
   return Math.random().toString(36).slice(2,8);
 };
 
+//some static data to test with
 let urlDatabase = {
   "b2xVn2": {longURL: "http://www.lighthouselabs.ca", userID: "" },
   "9sm5xK": {longURL: "http://www.google.com", userID: ""}
 };
 
+//some static data to test with
 let users = {
   "userRandomID": {
     id: "userRandomID",
@@ -36,10 +37,7 @@ let users = {
   }
 };
 
-
-// REPLACE ALL req.cookies["userID"] WITH req.session.userID
-//REPLACE ALL res.cookie("userID", users[key].id) WITH req.session.userID = "users[key].id"
-
+//get request to render the urls_index page
 app.get('/urls', (req, res) => {
   var userID = req.session.userID; 
   var user = users[userID];
@@ -47,7 +45,6 @@ app.get('/urls', (req, res) => {
   // conditional to pick only urls that were created by this user
   for (var key in urlDatabase) {
     if (urlDatabase[key].userID === userID) {
-
       newDatabase[key] = urlDatabase[key];
     }
   }
@@ -55,6 +52,7 @@ app.get('/urls', (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+//get request to render the urls_new page if logged in
 app.get("/urls/new", (req, res) => {
   var userID = req.session.userID;
   var user = users[userID];
@@ -62,10 +60,11 @@ app.get("/urls/new", (req, res) => {
     res.render("urls_new", {user: user});
     return;
   }
+  //if not logged in redirect to the login page
   res.redirect("/login");
-  // res.render("urls_new", {user: user});
 });
 
+//get request to render register page
 app.get("/register", (req, res) => {
   var userID = req.session.userID;
   var user = users[userID];
@@ -77,20 +76,20 @@ app.post("/register", (req, res) => {
   const password = req.body.password;
   let hashedPassword = require("bcrypt").hashSync(password, 10);
   const userID = random();
-
+  //if the email or password is empty, 400 code
   if(email == "" || password == "") {
     res.status(400).send("Please enter a valid email and password.");
     return;
   }
-
+  //if email is already in users database, 400 code
   for (key in users) {
     if (email === users[key].email) {
       res.status(400).send("This email is already registered.");
       return;
     }
-  }   
+  } 
+  //store email, hashed password, and userID in users database  
   users[userID] = {id: userID, email: req.body.email, password: hashedPassword};
-  console.log("users at userID", users[userID]);
   req.session.userID = users[userID].id;
   res.redirect("/urls");
 });
@@ -101,16 +100,18 @@ app.get("/login", (req, res) => {
 
 app.post("/urls", (req, res) => {
   const shortURL = random();
+  //update urlsDatabase with shortURL and associated longURL and userID who created it
   urlDatabase[shortURL] = {longURL: req.body.longURL, userID: req.session.userID};
-  console.log(urlDatabase); 
   res.redirect("/urls");        
 });
 
+//redirect to longURL using the shortURL
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
 });
 
+//render urls_show page to edit URLS only if logged in and userID matches the URL
 app.get("/urls/:id", (req, res) => {
   var userID = req.session.userID;
   var user = users[userID];
@@ -122,19 +123,17 @@ app.get("/urls/:id", (req, res) => {
   }
 });
 
+//if the user matched the user who made the shortURL, update the url in the database
 app.post("/urls/:id", (req, res) => {
   if (req.session.userID === urlDatabase[req.params.id].userID) {
     urlDatabase[req.params.id] = {longURL: req.body.longURL, userID: req.session.userID};
-    console.log(req.body.longURL);
     res.redirect("/urls");
     return;
   }
   res.send("You may not edit a URL that you have not created."); 
- 
-  // urlDatabase[req.params.id]=req.body.longURL;
-  // res.redirect("/urls");
 })
 
+//if the user matches the user who created the shortURL, delete that shortURL
 app.post("/urls/:key/delete", (req, res) => {
   if (req.session.userID === urlDatabase[req.params.key].userID) {
     delete urlDatabase[req.params.key];
@@ -144,6 +143,7 @@ app.post("/urls/:key/delete", (req, res) => {
   res.send("You may not delete a URL that you have not created.");
 })
 
+//check if email is in users database and check if passwords match
 app.post("/login", (req, res) => {
   for (var key in users) {
     if (req.body.email === users[key].email) {
@@ -160,10 +160,12 @@ app.post("/login", (req, res) => {
   res.status(403).send("Sorry, the email you have entered has not yet been registered.");
 });
 
+//reset cookies on logout
 app.post("/logout", (req, res) => {
-  req.session = null; // replaced res.clearCookie('userID')
+  req.session = null; 
   res.redirect("/urls");
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
